452e138541ea35508f282dd76b7f02bcc8a14635
Igor Babalich <igor.babalich@gmail.com>
2020-06-07 08:30:47 -0400

    First vue3 component

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    Hello!
+    <Capacity/>
+</div>
+</template>
+
+<script>
+import Capacity from "./Capacity.vue"
+
+export default {
+    components: { Capacity },
+    data:() => ({
+        modalOpen: false
+    }),
+    methods: {
+        toggleModal() {
+            this.modalOpen = !this.modalOpen;
+        }
+    }
+}
+</script>
